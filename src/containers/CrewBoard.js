6a0b93de1e1cd1b54e617e8a4f732b1c6fcbaae0
Anna <karlovskaya@gmail.com>
2018-12-16 14:43:20 -0800

    initial redux

--- src/containers/CrewBoard.js
+++ src/containers/CrewBoard.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import uuid from 'uuid';
+// import uuid from 'uuid';
+
+import { connect } from 'react-redux';
+
 
 import CrewMember from '../components/CrewMember/CrewMember';
 import MemberDetails from '../components/MemberDetails/MemberDetails';
@@ -9,72 +12,73 @@ import AddMember from '../components/AddMember/AddMember';
 import styles from './CrewBoard.module.scss';
 
 class CrewBoard extends Component {
-  constructor(props) {
-    super(props);
+  // constructor(props) {
+  //   super(props);
 
-    this.state = {
-      members: [],
-      selectedMember: null,
-      error: false,
-      loading: false
-    }
-  }
+  //   this.state = {
+  //     members: [],
+  //     selectedMember: null,
+  //     error: false,
+  //     loading: false
+  //   }
+  // }
 
-  componentDidMount() {
-    this.setState({
-      loading: true
-    });
+  // componentDidMount() {
+  //   this.setState({
+  //     loading: true
+  //   });
 
-    Promise.all([
-      axios.get('https://jsonplaceholder.typicode.com/users'),
-      axios.get('https://jsonplaceholder.typicode.com/posts')
-    ])
-    .then(response => {
-      // store only last 6 users
-      const members = response[0].data.slice(-6);
-      const texts = response[1].data;
-      // add to each member avatar image from robohash api and textBody from /posts
-      const updatedMembers = members.map(member => {
-        return {
-          ...member,
-          image: `https://robohash.org/${member.username}?set=set2`,
-          text: texts[member.id].body
-        }
-      });
+  //   Promise.all([
+  //     axios.get('https://jsonplaceholder.typicode.com/users'),
+  //     axios.get('https://jsonplaceholder.typicode.com/posts')
+  //   ])
+  //   .then(response => {
+  //     // store only last 6 users
+  //     const members = response[0].data.slice(-6);
+  //     const texts = response[1].data;
+  //     // add to each member avatar image from robohash api and textBody from /posts
+  //     const updatedMembers = members.map(member => {
+  //       return {
+  //         ...member,
+  //         image: `https://robohash.org/${member.username}?set=set2`,
+  //         text: texts[member.id].body
+  //       }
+  //     });
 
-      this.setState({
-        members: updatedMembers,
-        loading: false
-      });
-    })
-    .catch(error => {
-      this.setState({
-        error: true
-      });
-    });
-  }
+  //     this.setState({
+  //       members: updatedMembers,
+  //       loading: false
+  //     });
+  //   })
+  //   .catch(error => {
+  //     this.setState({
+  //       error: true
+  //     });
+  //   });
+  // }
 
-  clickEventHandler = id => {
-    const selectedMember = this.state.members.filter(member => id === member.id );
+  // clickEventHandler = id => {
+  //   const selectedMember = this.state.members.filter(member => id === member.id );
 
-    this.setState({
-      selectedMember
-    });
-  }
+  //   this.setState({
+  //     selectedMember
+  //   });
+  // }
 
   
 
   render() {
-    const { members, selectedMember, error, loading } = this.state;
+    const { members, selectedMember, error, loading } = this.props;
+    console.log(this.props.members);
 
-    const listOfMembers = members.map(item => (
-      <CrewMember key={item.id} 
-                  name={item.username} 
-                  image={item.image} 
-                  origin={item.address}
-                  clicked={() => this.clickEventHandler(item.id)}
-      />
-    ));
+    // const listOfMembers = members.map(item => (
+    //   <CrewMember key={item.id} 
+    //               name={item.username} 
+    //               image={item.image} 
+    //               origin={item.address}
+    //               clicked={() => this.clickEventHandler(item.id)}
+    //   />
+    // ));
 
     let content = <img src="loading.gif" className={styles.loading} alt="Loading icon"/>;
 
@@ -84,18 +88,17 @@ class CrewBoard extends Component {
           <section>
             <h1>Crew Members Board</h1> 
             <ul className={styles.membersContainer}>
-              { listOfMembers }
+              {/* { listOfMembers } */}
             </ul>
           </section>
-
           <section>
             <h2 style={{textAlign: 'center'}}>Selected Member Info</h2>
-            <MemberDetails member={selectedMember} />
+            {/* <MemberDetails member={selectedMember} /> */}
           </section>
 
           <section>
             <h2>Add New Member</h2>
-            <AddMember />
+            {/* <AddMember /> */}
           </section>
         </React.Fragment>
       )}
@@ -109,4 +112,18 @@ class CrewBoard extends Component {
   }
 }
 
-export default CrewBoard;
+// specify what piece of state needed here
+const mapStateToProps = state => {
+  return {
+    members: state.members
+  };
+}
+
+// pass 'dispatch' function
+const mapDispatchToProps = dispatch => {
+  return {
+    nameOfAction: () => dispatch({type: 'NAME_OF_ACTION'})
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CrewBoard);
---
+++ src/store/reducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+  members: [],
+  selectedMember: null,
+  error: false,
+  loading: false
+}
+
+
+const reducer  = (state = initialState, action) => {
+  return state;
+}
+
+export default reducer;
